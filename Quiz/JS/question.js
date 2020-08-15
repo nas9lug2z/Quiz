b9b510227cf95bf1cd8f12cc3716896f065486f4
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-08-15 21:29:01 +0200

    in progress.html structure,  main styles and layout done

---
+++ Quiz/JS/question.js
@@ -0,0 +1,9 @@
+export default function Question(question, choices, answerKey) {
+    this.question = question;
+    this.choices = choices;
+    this.answerKey = answerKey;
+}
+
+const q1 = new Question (
+    ""
+)
