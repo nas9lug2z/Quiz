28d759286885eb4f198ac57e44dc844f73ed6644
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-08-31 21:17:16 +0200

    if all child categories are checkes, the parent checks and unchecks automatically

--- My-Plan/js/app.js
+++ My-Plan/js/app.js
@@ -2,15 +2,42 @@ const mainTaskList = document.querySelectorAll(".task-name");
 const subTaskList = document.querySelectorAll(".subtask");
 const subsubTaskList = document.querySelectorAll(".subsubtask");
 
-
 function toggleDone(tasklist) {
-
     for (let item of tasklist) {
-        item.addEventListener ("click", (e) => item.classList.toggle("done"));
-    }
+        var counter = 0;
+        item.addEventListener ("click", (e) => {
+            item.classList.toggle("done");
+            if (item.classList.length === 2) {
+                counter++;
+                console.log(`one is checked and the counter is ${counter}`);
+                if (counter === 2) {
+                    console.log("all is checked")
+                    item.parentNode.previousElementSibling.classList.add("done");
+                }
+                else {
+                    item.parentNode.previousElementSibling.classList.remove("done");
+                }
+            }
+            else {
+                counter--;
+                console.log(`one is UNchecked and the counter is ${counter}`)
+                item.closest("li").classList.remove("done");
+                if (counter === 2) {
+                    console.log("all is checked")
+                    item.parentNode.previousElementSibling.classList.add("done");
+                }
+                else {
+                    item.parentNode.previousElementSibling.classList.remove("done");
+                }
+            };
+
+
+        });
+}
 }
 
-toggleDone(subTaskList);
+
+// toggleDone(subTaskList);
 toggleDone(subsubTaskList);
 // toggleDone(subTask);
-// toggleDone(subsubTask);
+// toggleDone(subsubTask)
